ca52aa903fa535a0847762239eb4cb3f66bad582
ankitcits <ankitpatel@Ankits-MBP.lan>
2024-08-08 05:19:31 -0700

    error resolve

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -11,13 +11,14 @@ const Hero = () => {
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
               <div className="mx-auto max-w-[800px] text-center">
-              <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-  Unleash the Power of <span style={{ color: 'orange' }}>A2Z</span> HMS for <span style={{ color: 'orange' }}>Smarter Healthcare</span> Management(HMS)
-</h1>
+                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                  Unleash the Power of <span style={{ color: 'orange' }}>A2Z</span> HMS for <span style={{ color: 'orange' }}>Smarter Healthcare</span> Management(HMS)
+                </h1>
 
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                Empower Your Hospital's Efficiency and Patient Care with MocDoc HMS – Trusted by 10k+ Healthcare Professionals Worldwide for Over 5 Years. 
+                  Empower Your Hospital&apos;s Efficiency and Patient Care with MocDoc HMS – Trusted by 10k+ Healthcare Professionals Worldwide for Over 5 Years.
                 </p>
+
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="/contact"
